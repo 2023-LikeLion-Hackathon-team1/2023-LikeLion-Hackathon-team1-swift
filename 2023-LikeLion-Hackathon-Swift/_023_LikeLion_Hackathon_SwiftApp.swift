c71e036e6da4c5c9c632bd2d2f2fb5c2c47961fe
healim01 <22000770@handong.edu>
2023-08-11 15:27:52 +0900

    base code

---
+++ 2023-LikeLion-Hackathon-Swift/_023_LikeLion_Hackathon_SwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  _023_LikeLion_Hackathon_SwiftApp.swift
+//  2023-LikeLion-Hackathon-Swift
+//
+//  Created by 최혜림 on 8/11/23.
+//
+
+import SwiftUI
+
+@main
+struct _023_LikeLion_Hackathon_SwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
